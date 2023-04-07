8fd157de1d92d17b2ff2347d267112f2ad446c24
V3SU <66057674+V3sU@users.noreply.github.com>
2023-04-08 00:03:53 +0300

    Update NewBookEntry.cs

--- NewBookEntry.cs
+++ NewBookEntry.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,63 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
 
-namespace LibraryManagementSystem.forms
+namespace LibraryManagementSystem.Forms
 {
-    public partial class NewBookEntry : Form
+    public partial class NewBookEntryForm : Form
     {
-        public NewBookEntry()
+        public NewBookEntryForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void OnPublishYearSelected(object sender, EventArgs e)
         {
-
+            // Handle the selection event for the publish year combo box
         }
 
-        private void label5_Click(object sender, EventArgs e)
+        private void OnTitleTextChanged(object sender, EventArgs e)
         {
-
+            // Handle the text changed event for the title text box
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void OnEntryDateChanged(object sender, EventArgs e)
         {
-
+            // Handle the value changed event for the entry date picker
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click_1(object sender, EventArgs e)
+        private void OnInsertButtonClick(object sender, EventArgs e)
         {
             try
             {
-                Connection CN = new Connection();
-                string bk_insert = "INSERT INTO bookTable (bookName, bookPublishedYear, writerName, quantity, category, entryDate) VALUES('" + bookTextBox.Text + "', '" + bookPublishYearComboBox.Text + "', '" + writerNameTextBox.Text + "', '" + quantityTextBox.Text + "', '" + categoryComboBox.Text + "', '" + entryDateTimePicker.Text + "'); ";
-                CN.thisConnection.Open();
-                MySqlCommand cmcd = new MySqlCommand(bk_insert, CN.thisConnection);
+                // Create a new database connection
+                using (var connection = new MySqlConnection("Your Connection String"))
+                {
+                    // Create a new command for inserting a book
+                    var insertBookCommand = new MySqlCommand
+                    {
+                        Connection = connection,
+                        CommandText = "INSERT INTO Books (Title, PublishYear, Author, Quantity, Category, EntryDate) " +
+                                      "VALUES (@Title, @PublishYear, @Author, @Quantity, @Category, @EntryDate)"
+                    };
 
-                cmcd.ExecuteNonQuery();
+                    // Add parameters to the command
+                    insertBookCommand.Parameters.AddWithValue("@Title", titleTextBox.Text);
+                    insertBookCommand.Parameters.AddWithValue("@PublishYear", publishYearComboBox.Text);
+                    insertBookCommand.Parameters.AddWithValue("@Author", authorTextBox.Text);
+                    insertBookCommand.Parameters.AddWithValue("@Quantity", int.Parse(quantityTextBox.Text));
+                    insertBookCommand.Parameters.AddWithValue("@Category", categoryComboBox.Text);
+                    insertBookCommand.Parameters.AddWithValue("@EntryDate", entryDatePicker.Value);
 
-                CN.thisConnection.Close();
-                ERRORLAvEL.Text = " Data Saved";
+                    // Open the connection, execute the command, and close the connection
+                    connection.Open();
+                    insertBookCommand.ExecuteNonQuery();
+                    connection.Close();
 
+                    // Show a success message
+                    errorLabel.Text = "Data saved successfully.";
+                }
             }
             catch (Exception ex)
             {
-                ERRORLAvEL.Text = ex.Message;
+                // Display the error message
+                errorLabel.Text = $"Error: {ex.Message}";
             }
-
         }
     }
 }
