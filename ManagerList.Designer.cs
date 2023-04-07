5031a0c4769b5a8d4aba43ec1e89728cda53898a
V3SU <66057674+V3sU@users.noreply.github.com>
2023-04-07 23:39:01 +0300

    Update ManagerList.Designer.cs

--- ManagerList.Designer.cs
+++ ManagerList.Designer.cs
@@ -1,91 +1,88 @@
-﻿namespace LibraryManagementSystem.forms
+namespace LibraryManagementSystem.forms
 {
-    partial class ManagerList
+    partial class ManagerListForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+            /// <summary>
+            /// Required designer variable.
+            /// </summary>
+            private System.ComponentModel.IContainer components = null;
+            /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            components.Dispose();
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.refresh = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe Script", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(195, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(245, 27);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "AdminPage: Manager\'s List";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(34, 94);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(571, 227);
-            this.dataGridView1.TabIndex = 3;
-            // 
-            // refresh
-            // 
-            this.refresh.Font = new System.Drawing.Font("Franklin Gothic Medium", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.refresh.Location = new System.Drawing.Point(34, 337);
-            this.refresh.Name = "refresh";
-            this.refresh.Size = new System.Drawing.Size(80, 27);
-            this.refresh.TabIndex = 4;
-            this.refresh.Text = "Refresh";
-            this.refresh.UseVisualStyleBackColor = true;
-            this.refresh.Click += new System.EventHandler(this.refresh_Click);
-            // 
-            // ManagerList
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 450);
-            this.Controls.Add(this.refresh);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "ManagerList";
-            this.Text = "AdminPageManagerList";
-            this.Load += new System.EventHandler(this.ManagerList_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-        }
-
-        #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button refresh;
+        base.Dispose(disposing);
     }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.titleLabel = new System.Windows.Forms.Label();
+        this.managersDataGridView = new System.Windows.Forms.DataGridView();
+        this.refreshButton = new System.Windows.Forms.Button();
+        ((System.ComponentModel.ISupportInitialize)(this.managersDataGridView)).BeginInit();
+        this.SuspendLayout();
+        // 
+        // titleLabel
+        // 
+        this.titleLabel.AutoSize = true;
+        this.titleLabel.Font = new System.Drawing.Font("Segoe Script", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        this.titleLabel.Location = new System.Drawing.Point(195, 51);
+        this.titleLabel.Name = "titleLabel";
+        this.titleLabel.Size = new System.Drawing.Size(245, 27);
+        this.titleLabel.TabIndex = 1;
+        this.titleLabel.Text = "AdminPage: Manager's List";
+        this.titleLabel.Click += new System.EventHandler(this.titleLabel_Click);
+        // 
+        // managersDataGridView
+        // 
+        this.managersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        this.managersDataGridView.Location = new System.Drawing.Point(34, 94);
+        this.managersDataGridView.Name = "managersDataGridView";
+        this.managersDataGridView.Size = new System.Drawing.Size(571, 227);
+        this.managersDataGridView.TabIndex = 3;
+        // 
+        // refreshButton
+        // 
+        this.refreshButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+        this.refreshButton.Location = new System.Drawing.Point(34, 337);
+        this.refreshButton.Name = "refreshButton";
+        this.refreshButton.Size = new System.Drawing.Size(80, 27);
+        this.refreshButton.TabIndex = 4;
+        this.refreshButton.Text = "Refresh";
+        this.refreshButton.UseVisualStyleBackColor = true;
+        this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+        // 
+        // ManagerListForm
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(638, 450);
+        this.Controls.Add(this.refreshButton);
+        this.Controls.Add(this.managersDataGridView);
+        this.Controls.Add(this.titleLabel);
+        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        this.Name = "ManagerListForm";
+        this.Text = "ManagerListForm";
+        this.Load += new System.EventHandler(this.ManagerListForm_Load);
+        ((System.ComponentModel.ISupportInitialize)(this.managersDataGridView)).EndInit();
+        this.ResumeLayout(false);
+        this.PerformLayout();
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label titleLabel;
+    private System.Windows.Forms.DataGridView managersDataGridView;
+    private System.Windows.Forms.Button refreshButton;
 }
